91135171001b7cc17773bc9d6f1890c99fa67d8a
James <jmborden@gmail.com>
2016-09-13 15:31:16 -0500

    framework for login/logout setup

--- src/portal_cljs/core.cljs
+++ src/portal_cljs/core.cljs
@@ -1,6 +1,12 @@
 (ns portal-cljs.core
-  (:require [portal-cljs.login :as login]))
+  (:require [portal-cljs.login :as login]
+            [portal-cljs.landing :as landing]))
 
 (defn ^:export login
   []
   (login/login))
+
+
+(defn ^:export init-app
+  []
+  (landing/init-landing))
