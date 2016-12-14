4aad010c75f1bc11cc4f5e3e59b6a1098e04d4fa
James <jmborden@gmail.com>
2016-12-14 10:55:56 -0600

    ability to edit users added

--- src/portal_cljs/users.cljs
+++ src/portal_cljs/users.cljs
@@ -28,22 +28,32 @@
                                      :editing? false
                                      :confirming? false
                                      :retrieving? false}
+                    :edit-user-state {:edit-user default-new-user
+                                      :confrming? false
+                                      :retrieving? false
+                                      :editing? false}
                     :form-target default-form-target}))
+(defn reset-editing-atoms!
+  []
+  (reset! (r/cursor state [:add-user-state :editing?]) false)
+  (reset! (r/cursor state [:edit-user-state :editing?]) false))
 
 (defn UserFormComp
   [{:keys [user errors]}]
   (fn [{:keys [user errors]}]
     (let [email (r/cursor user [:email])
           name (r/cursor user [:name])
-          phone-number (r/cursor user [:phone_number])]
+          phone-number (r/cursor user [:phone_number])
+          editing-current-user? (r/cursor state [:edit-user-state :editing?])]
       [:div {:class "row"}
-       [:div {:class "col-lg-4 col-sm-4"}
-        [FormGroup {:label "email"
-                    :errors (:email @errors)}
-         [TextInput {:value @email
-                     :placeholder "Email Address"
-                     :on-change #(reset! email
-                                         (utils/get-input-value %))}]]]
+       (when-not @editing-current-user?
+         [:div {:class "col-lg-4 col-sm-4"}
+          [FormGroup {:label "email"
+                      :errors (:email @errors)}
+           [TextInput {:value @email
+                       :placeholder "Email Address"
+                       :on-change #(reset! email
+                                           (utils/get-input-value %))}]]])
        [:div {:class "col-lg-4 col-sm-4"}
         [FormGroup {:label "name"
                     :errors (:name @errors)}
@@ -58,6 +68,133 @@
                      :placeholder "User's Phone Number"
                      :on-change #(reset! phone-number
                                          (utils/get-input-value %))}]]]])))
+
+;; This is a stub function in case transformations need to be added later
+(defn server-user->form-user
+  [user]
+  (let [{:keys [email name phone_number]} user]
+    user))
+
+(defn form-user->server-user
+  [user]
+  (let [{:keys [email name phone_number]} user]
+    user))
+
+(defn EditUserForm
+  [user]
+  (let [edit-user-state (r/cursor state [:edit-user-state])
+        retrieving? (r/cursor edit-user-state [:retrieving?])
+        confirming? (r/cursor edit-user-state [:confirming?])
+        editing? (r/cursor edit-user-state [:editing?])
+        errors (r/atom {})
+        alert-success (r/atom "")]
+    (fn [user]
+      (let [current-user user ; before changes made to the the user
+            edit-user (r/cursor edit-user-state [:edit-user])
+            ;; helper fns
+            diff-key-str {:name "Name"
+                          :phone_number "Phone Number"}
+            diff-msg-gen (fn [edit current]
+                           (utils/diff-message
+                            edit
+                            current
+                            (select-keys
+                             diff-key-str (concat (keys edit)
+                                                  (keys current)))))
+            diff-msg-gen-user (fn [edit-user current-user]
+                                (diff-msg-gen
+                                 (server-user->form-user
+                                  edit-user)
+                                 (server-user->form-user
+                                  current-user)))
+            confirm-msg (fn []
+                          [:div "The following changes will be made "
+                           (map (fn [el]
+                                  ^{:key el}
+                                  [:h4 el])
+                                (diff-msg-gen-user
+                                 (form-user->server-user
+                                  @edit-user)
+                                 current-user))])
+            submit-on-click (fn [e]
+                              (.preventDefault e)
+                              (if @editing?
+                                (if (every? nil?
+                                            (diff-msg-gen-user
+                                             @edit-user
+                                             current-user))
+                                  ;; there isn't a diff message,no changes
+                                  (reset! editing? false)
+                                  ;; there is a diff message, confirm changes
+                                  (reset! confirming? true))
+                                (do
+                                  ;; reset edit user
+                                  (reset! edit-user
+                                          (server-user->form-user
+                                           @user))
+                                  (reset! alert-success "")
+                                  (reset! editing? true))))
+            confirm-on-click (fn [_]
+                               (entity-save
+                                (form-user->server-user @edit-user)
+                                (str (datastore/account-manager-context-uri)
+                                     "/edit-user")
+                                "PUT"
+                                retrieving?
+                                (edit-on-success
+                                 {:entity-type "user"
+                                  :entity-get-url-fn
+                                  (fn [id]
+                                    (str
+                                     (datastore/account-manager-context-uri)
+                                     "/user/" id))
+                                  :edit-entity edit-user
+                                  :alert-success alert-success
+                                  :aux-fn
+                                  (fn []
+                                    (reset! confirming? false)
+                                    (reset! retrieving? false)
+                                    (reset! editing? false))})
+                                (edit-on-error edit-user
+                                               :aux-fn
+                                               (fn []
+                                                 (reset! confirming? false)
+                                                 (reset! retrieving? false)
+                                                 (reset! alert-success ""))
+                                               :response-fn
+                                               (fn [response]
+                                                 (reset! errors response)))))
+            dismiss-fn (fn [e]
+                         ;; reset any errors
+                         (reset! errors nil)
+                         ;; no longer editing
+                         (reset-editing-atoms!)
+                         ;; reset edit-user
+                         (reset! edit-user
+                                 current-user)
+                         ;; reset confirming
+                         (reset! confirming? false))]
+        [:div {:class "form-border"
+               :style {:margin-top "15px"}}
+         [:form
+          [UserFormComp {:user edit-user
+                         :errors errors}]
+          [:div {:class "row"}
+           [:div {:class "col-lg-6 col-sm-6"}
+            [SubmitDismissConfirmGroup
+             {:confirming? confirming?
+              :editing? editing?
+              :retrieving? retrieving?
+              :submit-fn submit-on-click
+              :dismiss-fn dismiss-fn
+              :edit-btn-content "Create User"}]
+            (when @confirming?
+              [ConfirmationAlert
+               {:confirmation-message confirm-msg
+                :cancel-on-click dismiss-fn
+                :confirm-on-click confirm-on-click
+                :retrieving? retrieving?}])]]]]))))
+
 (defn AddUserForm
   []
   (let [add-user-state (r/cursor state [:add-user-state])
@@ -121,7 +258,7 @@
                          ;; reset any errors
                          (reset! errors nil)
                          ;; no longer editing
-                         (reset! editing? false)
+                         (reset-editing-atoms!)
                          ;; reset edit-zone
                          (reset! new-user default-new-user)
                          ;; reset confirming
@@ -184,9 +321,8 @@
                       (utils/get-by-id
                        @datastore/users
                        (:id user)))
-              #_ (reset! form-target [EditUserForm
-                                      @edit-user])
-              (.log js/console "I would have edited a user"))}
+              (reset! form-target [EditUserForm
+                                   @edit-user]))}
         [:i {:class (str "fa fa-pencil-square-o fa-2 "
                          "fake-link")}]]
        (when-not (:is-manager user)
@@ -194,7 +330,6 @@
                active? (:active user)
                switch-status
                (fn [_]
-                 (.log js/console "I would have switched the status of user")
                  (put! datastore/modify-data-chan
                        {:topic "users"
                         :data [(assoc user :retrieving? true)]})
